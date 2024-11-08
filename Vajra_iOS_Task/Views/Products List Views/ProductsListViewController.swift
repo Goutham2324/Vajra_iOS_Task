6967358b349fe31f9502d2c93bf0b8e6e31986f3
Swejan Kothamasu <swejan@deskfactors.com>
2024-11-08 22:36:22 +0530

    added font style and pull to refresh in tableview

--- Vajra_iOS_Task/Views/Products List Views/ProductsListViewController.swift
+++ Vajra_iOS_Task/Views/Products List Views/ProductsListViewController.swift
@@ -13,12 +13,13 @@ class ProductsListViewController: UIViewController {
     @IBOutlet weak var productsTableView: UITableView!
     
     let viewModel = ProductsListViewModel()
+    let refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTableView()
         viewModel.delegate = self
-        viewModel.getProducts()
+        fetchProducts()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -28,6 +29,16 @@ class ProductsListViewController: UIViewController {
     func setUpTableView() {
         productsTableView.delegate = self
         productsTableView.dataSource = self
+        productsTableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+    }
+    
+    func fetchProducts() {
+        viewModel.getProducts()
+    }
+    
+    @objc func refreshData() {
+        fetchProducts()
     }
 }
 
@@ -37,6 +48,7 @@ extension ProductsListViewController: ProductsListViewModelProtocol {
     func didUpdateDataSource() {
         DispatchQueue.main.async {
             self.productsTableView.reloadData()
+            self.productsTableView.refreshControl?.endRefreshing()
         }
     }
 }
